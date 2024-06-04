3465f99e09fd4131876dac421b3098ca5060284c
Kevin <kevin.campbell@mastercard.com>
2024-06-04 14:56:05 +0100

    adding in a few filters

--- Sources/XCAFootballDataClient/FootballDataClient.swift
+++ Sources/XCAFootballDataClient/FootballDataClient.swift
@@ -49,7 +49,7 @@ public struct FootballDataClient {
     }
     
     public func fetchLiveScore(competitionId: Int, filterOption: FilterOption = .latest) async throws -> [Match] {
-        let url = baseURL + "/competitions/\(competitionId)/matches?status=LIVE,SCHEDULED"
+        let url = baseURL + "/competitions/\(competitionId)/matches?status=LIVE,SCHEDULED,IN_PLAY,PAUSED,FINISHED"
         let urlRequest = URLRequest(url: URL(string: url)!)
         let response: LiveMatchesResponse = try await fetchData(request: urlRequest)
         guard let matches = response.matches else {
